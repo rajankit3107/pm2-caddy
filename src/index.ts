7d914a173524f1cc80a0b2c3d0f53e7d0bc2f353
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-10-19 23:49:28 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello world");
+});
+
+app.get("/:id", (req: Request, res: Response) => {
+  res.send(`Hey ${req.params.id}`);
+});
+
+app.listen(3000, () => {
+  console.log(`server is running on port 3000`);
+});
